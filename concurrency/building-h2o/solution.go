fe09a8c3c0de87e3c807704ed74383647ff5ace9
Eldar Eyvazov <58527301+ProgrammerBuffalo@users.noreply.github.com>
2026-02-19 10:00:03 +0400

    Refactor H2O struct and implement channel logic

--- concurrency/building-h2o/solution.go
+++ concurrency/building-h2o/solution.go
@@ -8,18 +8,36 @@ import (
 type H2O struct {
 	ch1 chan struct{}
 	ch2 chan struct{}
+	ch3 chan struct{}
 }
 
 func NewH2O() *H2O {
-	return &H2O{}
+    ch2 := make(chan struct{}, 1)
+    ch2 <- struct{}{}
+	return &H2O{
+		ch1: make(chan struct{}, 1),
+		ch2: ch2,
+		ch3: make(chan struct{}, 1),
+	}
 }
 
 func (h *H2O) Hydrogen(releaseHydrogen func()) {
-	releaseHydrogen()
+	select {
+	case h.ch1 <- struct{}{}:
+		releaseHydrogen()
+        <-h.ch2
+		return
+	case h.ch2 <- struct{}{}:
+		releaseHydrogen()
+		h.ch3 <- struct{}{}
+		return
+	}
 }
 
 func (h *H2O) Oxygen(releaseOxygen func()) {
+	<-h.ch3
 	releaseOxygen()
+	<-h.ch1
 }
 
 func main() {
